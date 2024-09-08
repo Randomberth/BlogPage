ae9134ec42ae6c0d73c8478778c072d706d505fb
Alibert <aliberth33@gmail.com>
2024-09-08 19:02:02 -0400

    corregido HashRouter v1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base:'https://Randomberth.github.io/BlogPage/' 
-  base:'/'
+  base:'https://Randomberth.github.io/BlogPage/' 
+  //base:'/'
 })
